d73355e3412eeb0324a9fb32f4a536ac72a1fd0f
barnabasjacks-lab <barnabasjacks@gmail.com>
2026-09-15 11:32:15 +0300

    fix: solve ksp2 daemon build issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -136,5 +136,6 @@ dependencies {
   debugImplementation(libs.androidx.compose.ui.test.manifest)
   debugImplementation(libs.androidx.compose.ui.tooling)
   "ksp"(libs.androidx.room.compiler)
-  "ksp"(libs.moshi.kotlin.codegen)
+  // "ksp"(libs.moshi.kotlin.codegen)
 }
+
